b3dff82430b6ede67d46b809c8f5abe0178dbc15
jackhodgyuk <jackhodgyuk@gmail.com>
2025-01-08 17:24:57 +0000

    Initial Commit

---
+++ GigSync/GigSyncApp.swift
@@ -0,0 +1,32 @@
+//
+//  GigSyncApp.swift
+//  GigSync
+//
+//  Created by Jack Hodgy on 08/01/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GigSyncApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GigSyncTests/GigSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  GigSyncTests.swift
+//  GigSyncTests
+//
+//  Created by Jack Hodgy on 08/01/2025.
+//
+
+import Testing
+@testable import GigSync
+
+struct GigSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
